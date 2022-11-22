68b93df5bf3e0af0857a2eb2bf3b3cad1b0b746c
henrieessalu <henri.eessalu@gmail.com>
2022-11-22 10:19:57 +0200

    Initial commit

---
+++ src/main/java/com/example/iotsimulator/IoTSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iotsimulator/IoTSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
